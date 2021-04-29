58f6ca7579aa586c8809ed7de75930949c0d769c
OM Miles <olivia.m.miles@gmail.com>
2021-04-29 10:51:33 -0700

    lab complete

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+
+    state = {
+        timesclicked: 0
+    }
+
+    updateClick = () => {
+        this.setState(previousState => {
+            return {
+                timesclicked: previousState.timesclicked + 1
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <button
+                onClick={this.updateClick}
+            >
+                {this.state.timesclicked}
+            </button>
+        )
+    }
+}
+
